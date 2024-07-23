447fe2e2dc1d38971749926090440c02a337ef01
john <john@WinDev2404Eval>
2024-07-23 16:36:00 -0700

    摘要: TestCron version2 详细描述: - 更改原因 - 解决方法 - 影响范围 - 其他说明 Fixes # 问题编号

--- DimPlatform/TestCron/src/main/java/com/study/testCron/config/BusinessDataSourceConfig.java
+++ DimPlatform/TestCron/src/main/java/com/study/testCron/config/BusinessDataSourceConfig.java
@@ -1,26 +1,22 @@
 package com.study.testCron.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
 
 @Configuration
-@EnableJpaRepositories(
-        basePackages = "com.study.testCron.repository",
-        entityManagerFactoryRef = "businessEntityManagerFactory",
-        transactionManagerRef = "businessTransactionManager"
-)
+@MapperScan(basePackages = "com.study.testCron.mapper", sqlSessionTemplateRef  = "businessSqlSessionTemplate")
 public class BusinessDataSourceConfig {
 
     @Primary
@@ -31,28 +27,23 @@ public class BusinessDataSourceConfig {
     }
 
     @Primary
-    @Bean(name = "businessEntityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean businessEntityManagerFactory(
-            @Qualifier("businessDataSource") DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
-        em.setPackagesToScan("com.study.testCron.entity");
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-
-        HashMap<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto", "update");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
-        em.setJpaPropertyMap(properties);
-
-        return em;
+    @Bean(name = "businessSqlSessionFactory")
+    public SqlSessionFactory businessSqlSessionFactory(@Qualifier("businessDataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        return bean.getObject();
     }
 
     @Primary
     @Bean(name = "businessTransactionManager")
-    public PlatformTransactionManager businessTransactionManager(
-            @Qualifier("businessEntityManagerFactory") LocalContainerEntityManagerFactoryBean businessEntityManagerFactory) {
-        return new JpaTransactionManager(businessEntityManagerFactory.getObject());
+    public DataSourceTransactionManager businessTransactionManager(@Qualifier("businessDataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Primary
+    @Bean(name = "businessSqlSessionTemplate")
+    public SqlSessionTemplate businessSqlSessionTemplate(@Qualifier("businessSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
---
+++ DimPlatform/TestCron/src/main/java/com/study/testCron/model/User.java
@@ -0,0 +1,12 @@
+package com.study.testCron.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+
+    // getters and setters
+}
