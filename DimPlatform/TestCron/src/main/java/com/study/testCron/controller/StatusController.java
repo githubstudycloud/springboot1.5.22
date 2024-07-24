a6e2ca56154755b82156b105e613a0ec1d8b6c9f
john <john@WinDev2404Eval>
2024-07-24 15:34:22 -0700

    摘要: TestCron version2 详细描述: - 更改原因 - 解决方法 - 影响范围 - 其他说明 Fixes # 问题编号

--- DimPlatform/TestCron/src/main/java/com/study/testCron/controller/StatusController.java
+++
@@ -1,4 +0,0 @@
-package com.study.testCron.controller;
-
-public class StatusController {
-}
--- DimPlatform/TestCron/src/main/java/com/study/testCron/repository/JobExecutionHistoryRepository.java
+++
@@ -1,4 +0,0 @@
-package com.study.testCron.repository;
-
-public class JobExecutionHistoryRepository {
-}
